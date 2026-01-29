e4c04e0c8a110c155880488c8ce61c07ed5cb42d
S Kumar <Druva.SKumar@bottomline.com>
2026-01-29 12:19:28 +0530

    chore: update banner to v2

--- src/main/java/com/example/addressbook/App.java
+++ src/main/java/com/example/addressbook/App.java
@@ -2,9 +2,11 @@ package com.example.addressbook;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("   DevOps Pipeline Demo - v2");
         System.out.println("=================================");
         System.out.println("   Addressbook Application");
         System.out.println("   DevOps Pipeline Demo");
         System.out.println("=================================");
+
     }
 }
