5525a5a7e5b5df5867fdb395d0a2afa62e36978f
S Kumar <Druva.SKumar@bottomline.com>
2026-01-29 12:10:53 +0530

    Initial Addressbook Java application with Maven

---
+++ src/main/java/com/example/addressbook/App.java
@@ -0,0 +1,10 @@
+package com.example.addressbook;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("   Addressbook Application");
+        System.out.println("   DevOps Pipeline Demo");
+        System.out.println("=================================");
+    }
+}
